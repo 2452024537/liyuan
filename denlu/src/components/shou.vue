2e26a205850d2811f27e64468192b8250d04c09f
2452024537 <2452024537@qq.com>
2019-11-04 14:18:18 +0800

    'denlu'

---
+++ denlu/src/components/shou.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="shou">
+   
+  </div>
+</template>
+<script>
+export default {
+  name: 'shou',
+  data(){
+      return{
+         node:[
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"},
+                  {name:"1111111111111111111111111111"}
+              ]
+      }
+  },
+  mounted(){
+     window.addEventListener('scroll',this.watchscroll)
+  },
+  methods:{
+     watchscroll:function(){
+         var scrollTop = window.pageYOffset||document.documentElement.scrollTop||document.body.scrollTop;
+         if(scrollTop>50){
+            
+         }
+     }
+  },
+}
+</script>
+<style>
+.heads{
+  width:100%;
+  height:50px;
+  background: #000;
+}
+.active{
+   position: fixed;
+   top:50px;
+   z-index: 999;
+}
+.liu{
+  width:100%;
+  height:60px;
+  line-height: 40px;
+  text-align: center;
+}
+</style>
---
+++ denlu/src/router.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+Vue.use(Router)
+const routes = [
+      {name:"shou",path:"/shou",component:()=>import('./components/shou')},
+      {name:"denl",path:"/denl",component:()=>import('./components/denl')},
+      {name:"zuce",path:"/zuce",component:()=>import('./components/zuce')},
+]
+const router = new Router({
+      routes
+})
+export default router
